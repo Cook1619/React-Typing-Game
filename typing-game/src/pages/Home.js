14f823a2a6612eee2d7050f72efcc09d1f0eee55
Mcook <cook6348@gmail.com>
2020-12-08 08:14:39 -0600

    Styled homepage component

--- typing-game/src/pages/Home.js
+++ typing-game/src/pages/Home.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react';
+import CTA from '../styled/CTA';
+import { Accent, StyledTitle } from '../styled/Random';
 
 export default function Home() {
-  return <h1>Home Screen sadasd asdasd asdasdas</h1>;
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type <Accent>'s'</Accent> to start playing!!
+      </CTA>
+    </div>
+  );
 }
