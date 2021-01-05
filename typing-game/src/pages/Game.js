a6b465d24d3f2453b8dff2e298482ebb51681a95
Mathew Cook <cook6348@gmail.com>
2021-01-05 07:45:57 -0600

    Dev (#7)
    * Adding in styled components with base styles for app
    
    * Styled navbar
    
    * Styled homepage component
    
    * Styled game page
    
    * adding setState hook
    
    * Implemented timer hooks

--- typing-game/src/pages/Game.js
+++ typing-game/src/pages/Game.js
@@ -7,12 +7,55 @@ import {
   StyledCharacter,
 } from '../styled/Game';
 
+
+import { Strong } from '../styled/Random';
+
+export default function Game({ history }) {
+  const [score, setScore] = useState(0);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMSString = ('0000' + msPassedStr).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substr(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMSString.substring(formattedMSString.length - 3));
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = '';
+    for (let i = 0; i < length; i++) {
+      zeros += '0';
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push('gameOver');
+    }
+  }, [seconds, ms]);
+
 import { Strong } from '../styled/Random';
 
 export default function Game() {
   const [score, setScore] = useState(0);
 
   useEffect(() => {}, [score]);
+
   return (
     <StyledGame>
       <StyledScore>
@@ -20,7 +63,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyledTimer>
-        Time is: <Strong>00:00</Strong>
+        Time is:{' '}
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyledTimer>
     </StyledGame>
   );
